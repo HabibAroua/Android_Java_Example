90260f2913e925ecff12264c9954cb856c2be67c
HabibAroua <habibha.aroua82@gmail.com>
2019-04-25 22:22:23 +0100

    add new class MySqlHelper.java and XML file

--- app/src/main/java/com/example/formulaire/SqliteExample1.java
+++ app/src/main/java/com/example/formulaire/SqliteExample1.java
@@ -1,14 +1,19 @@
 package com.example.formulaire;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SqliteExample1 extends AppCompatActivity
 {
+    SQLiteDatabase bdd;
+    MySqliteHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sqlite_example1);
+        helper=new MySqliteHelper(this);
+        bdd
     }
 }
